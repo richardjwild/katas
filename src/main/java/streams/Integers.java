176b59222b08ebf1f0fc528bff59665dc9f3e316
Richard Wild <richard.wild@e2y.co.uk>
2017-04-18 18:06:38 +0100

    Use Stream.generate to avoid having to implement all the methods we don't want

--- src/main/java/streams/Integers.java
+++ src/main/java/streams/Integers.java
@@ -1,13 +1,11 @@
 package streams;
 
-import java.util.Collection;
-import java.util.Iterator;
 import java.util.stream.Stream;
 
-public class Integers implements Collection<Integer> {
+public class Integers {
 
     public static Stream<Integer> from(int from) {
-        return new Integers(from).stream();
+        return new Integers(from).getStream();
     }
 
     private int value;
@@ -16,87 +14,7 @@ public class Integers implements Collection<Integer> {
         value = from;
     }
 
-    @Override
-    public int size() {
-        return Integer.MAX_VALUE;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return true;
-    }
-
-    @Override
-    public Iterator<Integer> iterator() {
-        return new Integerator(value);
-    }
-
-    @Override
-    public Object[] toArray() {
-        return new Object[0];
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        return null;
-    }
-
-    @Override
-    public boolean add(Integer integer) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return true;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends Integer> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    private class Integerator implements Iterator<Integer> {
-
-        private int value;
-
-        public Integerator(int from) {
-            value = from;
-        }
-
-        @Override
-        public boolean hasNext() {
-            return true;
-        }
-
-        @Override
-        public Integer next() {
-            return value++;
-        }
+    private Stream<Integer> getStream() {
+        return Stream.generate(() -> value++);
     }
 }
